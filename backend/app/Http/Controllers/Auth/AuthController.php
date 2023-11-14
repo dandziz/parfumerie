fcf7025ec8f2a701967f1e6c5df1fdc140e99c2b
dandziz <Daodan2612@gmail.com>
2023-11-14 22:49:05 +0700

    init
    First Update

---
+++ backend/app/Http/Controllers/Auth/AuthController.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Auth\LoginPostRequest;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    public function login(LoginPostRequest $request): JsonResponse
+    {
+        $input = $request->validated();
+        $check = Auth::attempt($input);
+        if ($check) {
+            $user = Auth::user();
+            $token = $user->createToken('example')->accessToken;
+            return response()->json(['status' => 200, 'data' => [
+                'user' => $user,
+                'token' => $token
+            ]], 200);
+        } else {
+            return response()->json(['status' => 401, 'message' => [
+                'Invalid credentials!'
+            ]], 401);
+        }
+    }
+
+    public function getUserDetail(): JsonResponse
+    {
+        $user = Auth::guard('api')->user();
+        return response()->json(['status' => 200, 'message' => ['Get user successfully!'], 'data' => $user], 200);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(User $user)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, User $user)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(User $user)
+    {
+        //
+    }
+}
---
+++ backend/app/Http/Requests/Auth/LoginPostRequest.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Requests\Auth;
+
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Validation\ValidationException;
+
+class LoginPostRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            "email"=> [
+                "required",
+                "string"
+            ],
+            "password"=> [
+                "required",
+                "string"
+            ],
+        ];
+    }
+
+    protected function failedValidation(Validator $validator) 
+    {
+
+        $errors = (new ValidationException($validator))->errors();
+        throw new HttpResponseException(response()->json(
+            [
+                'error' => $errors,
+                'status_code' => 422,
+            ], JsonResponse::HTTP_UNPROCESSABLE_ENTITY));
+    }
+
+}
